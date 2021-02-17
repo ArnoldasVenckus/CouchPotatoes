49c7e37f99c98e06929b18ea8aaf24e3c7e39c10
Karolis-Kvatavicius <karolis.kvatavicius@gmail.com>
2021-02-17 14:44:05 +0200

    API carousel

--- js/main.js
+++ js/main.js
@@ -10,10 +10,38 @@
 "use strict";
 
 (function ($) {
+
+  let links = [];
+
+  $.ajax({
+    url: "http://api.tvmaze.com/schedule/web",
+    async: false,
+    success: function (result) {
+      result.slice(0, 5).forEach(element => {
+        // console.log(element);
+        // console.log(links);
+        links.push(element._embedded.show);
+      });
+    }
+  });
+
   /*------------------
         Preloader
     --------------------*/
   $(window).on("load", function () {
+
+        console.log(links);
+
+    let abc = Array.from(document.querySelectorAll(".set-bg")).slice(3, 8);
+    console.log(abc);
+    abc.forEach((carouselItem, index) => {
+      // console.log(links);
+      carouselItem.setAttribute('data-setbg', links[index].image.original);
+      carouselItem.setAttribute('style', "background-image: url('"+links[index].image.original+"');");
+    });
+
+    // console.log([links, abc]);
+
     $(".loader").fadeOut();
     $("#preloder").delay(200).fadeOut("slow");
 
@@ -106,7 +134,9 @@
         Scroll To Top
     --------------------*/
   $("#scrollToTopButton").click(function () {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow");
     return false;
   });
 })(jQuery);
